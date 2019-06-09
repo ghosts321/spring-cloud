da04e674478b398ff48a3426be913fb1389f83fc
ghosts <864867103@qq.com>
2019-06-09 16:53:21 +0800

    集成log4j2

--- configclient/src/main/java/com/spring/cloud/configclient/rabbitmq/receiver/DirectReceiver.java
+++ configclient/src/main/java/com/spring/cloud/configclient/rabbitmq/receiver/DirectReceiver.java
@@ -2,8 +2,10 @@ package com.spring.cloud.configclient.rabbitmq.receiver;
 
 import com.spring.cloud.configclient.rabbitmq.config.RabbitConfig;
 import com.spring.cloud.configclient.rabbitmq.entity.User;
+import org.jboss.logging.Logger;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
 
 /**
  *
@@ -12,16 +14,22 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class DirectReceiver {
+    
+    /**
+     * logger 
+     */
+    private static final Logger LOGGER = Logger.getLogger(DirectReceiver.class);
+    
     // queues是指要监听的队列的名字
     @RabbitListener(queues = RabbitConfig.DIRECT_QUEUE1)
     public void receiveDirect1(User user) {
 
-        System.out.println("【receiveDirect1监听到消息】" + user);
+        LOGGER.info("【receiveDirect1监听到消息】" + user);
     }
 
     @RabbitListener(queues = RabbitConfig.DIRECT_QUEUE1)
     public void receiveDirect2(User user) {
 
-        System.out.println("【receiveDirect2监听到消息】" + user);
+        LOGGER.info("【receiveDirect2监听到消息】" + user);
     }
 }
--- configclient/src/main/java/com/spring/cloud/configclient/rabbitmq/receiver/TopicReceiver.java
+++ configclient/src/main/java/com/spring/cloud/configclient/rabbitmq/receiver/TopicReceiver.java
@@ -2,6 +2,7 @@ package com.spring.cloud.configclient.rabbitmq.receiver;
 
 import com.spring.cloud.configclient.rabbitmq.config.RabbitConfig;
 import com.spring.cloud.configclient.rabbitmq.entity.User;
+import org.jboss.logging.Logger;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -12,13 +13,20 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class TopicReceiver {
+
+    /**
+     * logger
+     */
+    private static final Logger LOGGER = Logger.getLogger(TopicReceiver.class);
+
     // queues是指要监听的队列的名字
     @RabbitListener(queues = RabbitConfig.TOPIC_QUEUE1)
     public void receiveTopic1(User user) {
-        System.out.println("【receiveTopic1监听到消息】" + user.toString());
+
+        LOGGER.info("【receiveTopic1监听到消息】" + user.toString());
     }
     @RabbitListener(queues = RabbitConfig.TOPIC_QUEUE2)
     public void receiveTopic2(User user) {
-        System.out.println("【receiveTopic2监听到消息】" + user.toString());
+        LOGGER.info("【receiveTopic2监听到消息】" + user.toString());
     }
 }
